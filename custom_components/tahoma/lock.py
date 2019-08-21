9060014f2e2c0b183d313f43953e2eedaf6a3f5c
vlb <vlebourl@gmail.com>
2019-08-21 11:36:09 +0200

    Added support for somfy lock in tahoma.

--- custom_components/tahoma/lock.py
+++ custom_components/tahoma/lock.py
@@ -16,7 +16,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Tahoma covers."""
     controller = hass.data[TAHOMA_DOMAIN]['controller']
     devices = []
-    for device in hass.data[TAHOMA_DOMAIN]['devices']['cover']:
+    for device in hass.data[TAHOMA_DOMAIN]['devices']['lock']:
         devices.append(TahomaLock(device, controller))
     add_entities(devices, True)
 
@@ -28,10 +28,18 @@ class TahomaLock(TahomaDevice, LockDevice):
         super().__init__(tahoma_device, controller)
         self._state = STATE_LOCKED
         self._available = False
+        self._battery_level = None
+        self._name = None
 
     def update(self):
         """Update method."""
         self.controller.get_states([self.tahoma_device])
+        self._battery_level = self.tahoma_device.active_states['core:BatteryState']
+        self._name = self.tahoma_device.active_states['core:NameState']
+        if self._battery_level == 'low':
+            _LOGGER.warning("Lock "+self._name+" has low battery")
+        if self._battery_level == 'verylow':
+            _LOGGER.error("Lock " + self._name + " has very low battery")
         if self.tahoma_device.active_states.get('core:LockedUnlockedState') == 'locked':
             self._state = STATE_LOCKED
         else:
@@ -42,13 +50,22 @@ class TahomaLock(TahomaDevice, LockDevice):
         pass
 
     def unlock(self, **kwargs):
-        pass
+        _LOGGER.info("unlocking ",self._name)
+        self.apply_action('unlock')
 
     def lock(self, **kwargs):
-        pass
+        _LOGGER.info("locking ",self._name)
+        self.apply_action('lock')
 
-    # @property
-    # def is_locked(self):
+    @property
+    def is_locked(self):
+        return self._state == STATE_LOCKED
+
+    @property
+    def state(self):
+        """Return the state."""
+        print(self._state)
+        return self._state
 
     @property
     def device_state_attributes(self):
@@ -57,10 +74,10 @@ class TahomaLock(TahomaDevice, LockDevice):
         super_attr = super().device_state_attributes
         if super_attr is not None:
             attr.update(super_attr)
-        for x in self.tahoma_device.active_states:
-            print(x)
-            for y in self.tahoma_device.active_states[x]:
-                print(y,':',self.tahoma_device.active_states[x][y])
-        # attr[ATTR_BATTERY_LEVEL] = self.tahoma_device.active_states['core:BatteryState']
+        attr[ATTR_BATTERY_LEVEL] = \
+            self.tahoma_device.active_states['core:BatteryState']
+        attr['availability'] = \
+            self.tahoma_device.active_states['core:AvailabilityState']
+        attr['name'] = \
+            self.tahoma_device.active_states['core:NameState']
         return attr
-
